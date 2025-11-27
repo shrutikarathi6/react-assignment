ad6768ea07c76dea730c778b6fa8ed03bf5e732a
shrutikarathi6 <shrutika28rathi@gmail.com>
2025-11-27 20:54:58 +0530

    Added Context for Course

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -2,26 +2,28 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
-import Admin from './pages/Admin';
 import CourseExplorer from './pages/CourseExplorer';
+import Admin from './pages/Admin';
+import CourseCreation from './pages/CourseCreation';
+import { CourseProvider } from './context/CourseContext';
 
 function App() {
   return (
- 
+    <CourseProvider>
       <Router>
         <div className="min-h-screen bg-gray-50 flex flex-col">
           <Header />
           <main className="flex-1">
             <Routes>
-             <Route path="/" element={<CourseExplorer />} />
+              <Route path="/" element={<CourseExplorer />} />
               <Route path="/admin" element={<Admin />} />
-              
+              <Route path="/create" element={<CourseCreation />} />
             </Routes>
           </main>
           <Footer />
         </div>
       </Router>
-   
+    </CourseProvider>
   );
 }
 
