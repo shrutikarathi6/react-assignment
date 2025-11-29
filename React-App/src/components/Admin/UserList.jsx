241d583f292af3a0142c128f42c17f5000308fb2
shrutikarathi6 <shrutika28rathi@gmail.com>
2025-11-29 20:05:04 +0530

    Small change in userlist

--- React-App/src/components/Admin/UserList.jsx
+++ React-App/src/components/Admin/UserList.jsx
@@ -44,7 +44,7 @@ const UserList = ({ users }) => {
                                 Role
                             </th>
                             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Status
+                                Courses
                             </th>
                         </tr>
                     </thead>
